8c00ea0fbcdd287798af30554b83a6b839874e3c
Maxime Caly <max.caly@gmail.com>
2019-03-18 17:25:13 -0700

    Update and clean test

--- server/test/routes/index.js
+++ server/test/routes/index.js
@@ -1,12 +1,9 @@
-var supertest = require('supertest');
-var chai = require('chai');
-var app = require('../../server.js');
 
 describe('GET /jobs', function() {
   it('returns a list of jobs', function(done) {
     request.get('/jobs').expect(200).end((err, res) => {
       const { jobs } = res.body;
-      expect(res.body.jobs.length > 0)
+      expect(jobs.length > 0)
       done();
     })
   })
